8557da97143716607e62849bd57cedbd1e2457d4
Manasvi Arya <manasvi.arya@ectl.in>
2022-07-18 13:05:14 +0530

    initial commit

---
+++ sum.js
@@ -0,0 +1,10 @@
+// JavaScript Program To Add Two Numbers
+// Define variable with integers
+var num1 = 2; 
+var num2 = 3;
+
+// add two numbers
+var sum = num1 + num2;
+
+// display output
+console.log("The Sum of " + num1 + " and " + num2 + " is: " + sum);
