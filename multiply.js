937e2d945b3e3489c2937d2a391517d8a40a6456
Manasvi Arya <manasvi.arya@ectl.in>
2022-07-18 15:19:53 +0530

    adding multiply.js

---
+++ multiply.js
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <body>
+            <h1> Lets Learn Git</h1>
+           
+        </body>
+        <script>
+                var a = 2;
+                var b = 3;
+                var c = a*b;
+                document.write(c);
+            </script>
+    </head>
+</html>
