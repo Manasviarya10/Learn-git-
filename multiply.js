c3c6a3656bf453d4a66c705b76dbc906236a4b95
Manasviarya10 <109572567+Manasviarya10@users.noreply.github.com>
2022-07-20 09:54:57 +0530

    Update multiply.js

--- multiply.js
+++ multiply.js
@@ -4,7 +4,7 @@
             <h1> Lets Learn Git</h1>
            
         </body>
-        <script>
+        <script>3650650505
                 var a = 2;
                 var b = 3;
                 var c = a*b;
